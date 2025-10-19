9695bf5d184503134f11f088f7d479196236d926
baby4dm <babych.dmtr@gmail.com>
2025-10-19 23:54:02 +0300

    added hover effects

--- index.js
+++ index.js
@@ -320,6 +320,22 @@ function resetBoard() {
   }
 }
 
+cells.forEach((cell, i) => {
+  cell.addEventListener("mouseenter", () => {
+    if (!board[i]) {
+      cell.classList.add(playerMark === "X" ? "x-hover" : "o-hover");
+      cell.innerHTML = playerMark === "X" ? getXSVGOutline() : getOSVGOutline();
+    }
+  });
+
+  cell.addEventListener("mouseleave", () => {
+    if (!board[i]) {
+      cell.classList.remove("x-hover", "o-hover");
+      cell.innerHTML = "";
+    }
+  });
+});
+
 function getXSVG() {
   return `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
@@ -340,5 +356,30 @@ function getOSVG() {
         fill="#F2B137"
       />
     </svg>
+  `;
+}
+function getXSVGOutline() {
+  return `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 68 68" width="64" height="64">
+    <path
+      d="M51.12 1.269c.511 0 1.023.195 1.414.586l9.611 9.611c.391.391.586.903.586 1.415s-.195 1.023-.586 1.414L44.441 32l17.704 17.705c.391.39.586.902.586 1.414 0 .512-.195 1.024-.586 1.415l-9.611 9.611c-.391.391-.903.586-1.415.586a1.994 1.994 0 0 1-1.414-.586L32 44.441 14.295 62.145c-.39.391-.902.586-1.414.586a1.994 1.994 0 0 1-1.415-.586l-9.611-9.611a1.994 1.994 0 0 1-.586-1.415c0-.512.195-1.023.586-1.414L19.559 32 1.855 14.295a1.994 1.994 0 0 1-.586-1.414c0-.512.195-1.024.586-1.415l9.611-9.611c.391-.391.903-.586 1.415-.586s1.023.195 1.414.586L32 19.559 49.705 1.855c.39-.391.902-.586 1.414-.586Z"
+      stroke="#31C3BD"
+      stroke-width="2"
+      fill="none"
+    />
+  </svg>
+  `;
+}
+
+function getOSVGOutline() {
+  return `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 68 68" width="64" height="64">
+    <path 
+      d="M33 1c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C15.327 65 1 50.673 1 33 1 15.327 15.327 1 33 1Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z"
+      stroke="#F2B137"
+      stroke-width="2"
+      fill="none"
+    />
+  </svg>
   `;
 }
